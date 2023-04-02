46dcf73115d6cc3e19b97eb217d38cc47d8c34ee
Santosh Kumar Malik <santoshkumarmalik473@gmail.com>
2023-04-02 15:58:47 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
   const [loading, setLoading] = useState(true);
- const API_KEY="b9eea9ddd4dbb47bf0bb1dfc76109bda";
+ const API_KEY="1db984782c58d9411b62400464075793";
 
  const changeFunction=(e)=>
  {
